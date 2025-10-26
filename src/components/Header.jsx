fc515b544bed5b85bfd7f20af839430646f4166d
Melnychuk Igor <igor.melnychuck@gmail.com>
2025-10-26 10:43:43 +0200

    make the current amount of goods in the cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useContext  } from "react";
 import { Link } from 'react-router-dom';
 
 import "macro-css";
@@ -7,7 +8,12 @@ import heartImg from "@/assets/heart.svg";
 import cartImg from "@/assets/cart.svg";
 import userImg from "@/assets/user.svg";
 
+import {AppContext} from '../App';
+
 function Header({ onClickCart }) {
+  const { cartItems } = useContext(AppContext);
+  const totalPrice = cartItems.reduce((sum, obj) => obj.price + sum, 0);
+
   return (
     <header className="d-flex justify-between align-center p-40">
       <Link to="/">
@@ -22,7 +28,7 @@ function Header({ onClickCart }) {
       <ul className="d-flex">
         <li onClick={onClickCart} className="mr-30 cu-p">
           <img width={18} height={18} src={cartImg} alt="Cart" />
-          <span>15 $</span>
+          <span>{totalPrice}$</span>
         </li>
         <li className="mr-20 cu-p">
           <Link to="/favorites">
